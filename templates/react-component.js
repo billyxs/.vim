a323f114ef1023c9e9c1f7f9ae0db0d3ac99494f
Billy Montgomery <billy.montgomery@gmail.com>
2017-09-25 11:24:35 -0700

    add templates for test and react components

---
+++ templates/react-component.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const Component = ({
+
+}) => (
+  <div>
+    {children}
+  </div>
+)
+
+Component.displayName = 'Component'
+
+Component.propTypes = {
+
+}
+
+Component.defaultProps = {
+
+}
+
+export default Component
